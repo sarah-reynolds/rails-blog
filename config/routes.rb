2edd93b368787ec76d292efb5d44bf4775b04969
Sarah Basinger <sarrey@gmail.com>
2017-11-17 14:13:39 -0500

    Added category create and integration test

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :categories, except: [:destroy]
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
